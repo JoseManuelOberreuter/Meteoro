98c73acaca876f4f48e0f1cd136e2925a8344013
Jose Manuel Oberreuter <jose.manuel.oberreuter@gmail.com>
2025-07-08 10:58:03 -0400

    update: replacing img sessions

--- src/data/sessions.js
+++ src/data/sessions.js
@@ -1,23 +1,27 @@
+import still1 from '@/assets/Still 2025-04-14 191143_1.2.1.jpg'
+import still2 from '@/assets/Still 2025-04-14 191143_1.3.2.jpg'
+import still3 from '@/assets/Still 2025-04-14 191143_2.1.2.jpg'
+
 export const sessions = [
   { 
     id: 1, 
     title: 'LIVE SESSION #01', 
     artist: 'LUNA ECLIPSE', 
     date: '2024',
-    image: 'https://images.unsplash.com/photo-1470229722913-7c0e2dbbafd3?q=80&w=2070'
+    image: still1
   },
   { 
     id: 2, 
     title: 'ACOUSTIC SERIES', 
     artist: 'CYBER FOLK', 
     date: '2024',
-    image: 'https://images.unsplash.com/photo-1598488035139-bdbb2231ce04?q=80&w=2070'
+    image: still2
   },
   { 
     id: 3, 
     title: 'UNDERGROUND VIBES', 
     artist: 'DARK MATTER', 
     date: '2024',
-    image: 'https://images.unsplash.com/photo-1574169208507-84376144848b?q=80&w=2079'
+    image: still3
   }
 ] 
